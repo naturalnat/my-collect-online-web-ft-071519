c09b856d3cdb7918e84d917a3ea9d3a0ef17f917
Natalie Taktachev <ntaktach@gmail.com>
2019-07-22 04:30:02 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  i = 0
+  collect= []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+   end
+  collect
+end
 
-
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
